69999903476bb4d2226f0b3e53330fbe66280da6
jamiemo <jamie@jamiemorrison.com>
2022-11-28 17:11:55 +0800

    Remove 429 - Too many requests

--- lib/common.rb
+++ lib/common.rb
@@ -1057,13 +1057,6 @@ def rest_client_exception(e, method, url, payload = {})
     message = e.to_s
   end
   puts "#{method} #{url}#{payload.empty? ? '' : ' ' + payload.inspect} => NOK (#{message})"
-  unless e.response.nil?
-    if e.response.include? "429 - Too many requests"
-        puts "429 Too Many Requests: Backoff 5 minutes"
-        puts "Response (#{e.response})"
-        sleep(300)
-    end
-  end
   message
 end
 
